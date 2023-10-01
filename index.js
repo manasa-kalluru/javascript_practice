4a78b9df715a79b8c4cdb0330a13f5b349a0b052
Manasa Kalluru <kallurma@mail.uc.edu>
2023-10-01 13:55:10 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let count =0;
+let identifier = setInterval(function interval(){
+    console.log("interval set", ++count);
+    if(count === 5){
+        clearInterval(identifier);
+    }
+    
+}, 100);
+
+
+// setTimeout(function clearInt(){
+//     clearInterval(identifier);
+// },10000);
+
+
+// let timeout = setTimeout(function a(){
+
+//     console.log("set timeout called");
+
+// },0);
+
+// console.log("print",timeout);
+// clearTimeout(timeout);
+
+
