61edd449b7dbf34bec7bef6faa8a716208220a43
Manasa Kalluru <kallurma@mail.uc.edu>
2023-10-01 14:59:13 -0400

    Second Commit

---
+++ ArrayFunctions/filter.js
@@ -0,0 +1,13 @@
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+const odd = arr.filter(function oddEle(element) {
+    if(element %2) 
+    return element;
+});
+console.log("odd is ", odd);
+
+for(i=0; i < arr.length; i++) {
+    if(arr[i] % 2){
+        console.log(arr[i]);
+    }
+}
