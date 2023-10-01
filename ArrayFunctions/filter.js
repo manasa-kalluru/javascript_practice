26b45a2e2514237a3cfa6483065552d1f4ef6aed
Manasa Kalluru <kallurma@mail.uc.edu>
2023-10-01 15:08:50 -0400

    filter function modified

--- ArrayFunctions/filter.js
+++ ArrayFunctions/filter.js
@@ -1,13 +1,16 @@
 const arr = [1,2,3,4,5,6,7,8,9,10];
 
 const odd = arr.filter(function oddEle(element) {
-    if(element %2) 
-    return element;
+    return element%2;
 });
+
+const oddArrFn = arr.filter((ele) => ele % 2 !== 0);
+
+
 console.log("odd is ", odd);
 
 for(i=0; i < arr.length; i++) {
-    if(arr[i] % 2){
+    if(Boolean(arr[i] % 2)){
         console.log(arr[i]);
     }
 }
