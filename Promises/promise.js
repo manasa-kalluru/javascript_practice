7c71c9492e7839a65724a0f31a2ab7d451aec1ec
manasa-kalluru <mns90852@gmail.com>
2023-10-05 23:49:03 -0400

    promises practise

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,2 +1,22 @@
 const gitAPI = "https://api.github.com/users/akshaymarch7";
 const user = fetch(gitAPI);
+
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+  }
+  
+
+fetchData('https://api.github.com/users/akshaymarch7')
+  .then(data => console.log('Data received:', data))
+  .catch(error => console.error('Error:', error));
+
