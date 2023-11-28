aeff1da477e491bdc57a937e092aa684746f27be
manasa-kalluru <mns90852@gmail.com>
2023-11-27 23:59:42 -0500

    SortBy

---
+++ sortBy.js
@@ -0,0 +1,16 @@
+var sortBy = function(arr, fn) {
+    var n = arr.length;
+    
+    for (var i = 0; i < n - 1; i++) {
+        for (var j = 0; j < n - i - 1; j++) {
+            if (fn(arr[j]) > fn(arr[j + 1])) {
+                // Swap arr[j] and arr[j+1]
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    return arr;
+};
